02547234a4f62a940d9ccc88cef5a6d15218c2a6
Guillermo de la Iglesia <zguillez@gmail.com>
2021-11-11 11:06:49 +0100

    v0.1.25

--- gotools.go
+++ gotools.go
@@ -3,11 +3,33 @@ package gotools
 import (
 	"github.com/fatih/color"
 	"github.com/zguillez/go-tools/core"
+	"github.com/zguillez/go-tools/files"
+	"github.com/zguillez/go-tools/geometry"
+	"github.com/zguillez/go-tools/image"
+	"github.com/zguillez/go-tools/text"
 )
 
 func Help() {
 	core.Help()
 }
 func Version() {
-	color.Green("v0.1.24")
+	color.Green("v0.1.25")
+}
+
+func Command(command string, args ...string) {
+	color.Yellow("[command: %v]", command)
+	switch command {
+	case "rectangle":
+		geometry.Rectangle()
+	case "replace":
+		text.ReplaceCli()
+	case "readfile":
+		files.ReadFile(args[0])
+	case "minimize":
+		image.Minimize(args[0], args[1], args[2])
+	case "version":
+		core.Version(args[0])
+	default:
+		color.Red("[error: command %v unknown]", command)
+	}
 }
